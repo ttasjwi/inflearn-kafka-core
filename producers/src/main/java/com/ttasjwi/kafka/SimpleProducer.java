78c152293680eb8b335dd5cdab0cb345329ff96f
ttasjwi <ttasjwi920@gmail.com>
2023-07-08 16:43:21 +0900

    Java 기반에서 Producer 구현 - 기본 비동기방식(Async)
    - 비동기적으로 메시지가 별도의 스레드에서 전송된다.

--- producers/src/main/java/com/ttasjwi/kafka/SimpleProducer.java
+++ producers/src/main/java/com/ttasjwi/kafka/SimpleProducer.java
@@ -1,8 +1,40 @@
 package com.ttasjwi.kafka;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+import static org.apache.kafka.clients.producer.ProducerConfig.*;
+
 public class SimpleProducer {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // 프로듀서 설정
+        KafkaProducer<String, String> kafkaProducer = setupKafkaProducer();
+
+        // 토픽명
+        String topicName = "simple-topic";
+
+        // 보낼 메시지(레코드)
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topicName,  "hello world");
+
+        // 메시지 전송
+        kafkaProducer.send(producerRecord);
+
+        kafkaProducer.flush();
+        kafkaProducer.close();
+    }
+
+    private static KafkaProducer<String, String> setupKafkaProducer() {
+        Properties props = new Properties();
+
+        // bootstrap.servers, key.serializer.class, value.serializer.class
+        props.setProperty(BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        props.setProperty(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.setProperty(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        return new KafkaProducer<>(props);
     }
 }
